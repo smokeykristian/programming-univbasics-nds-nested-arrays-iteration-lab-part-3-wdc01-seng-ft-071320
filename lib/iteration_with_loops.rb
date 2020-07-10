a6379f316babaa0272a1f42c985bf26dd33f8683
Kristian Portillo <portillk@gmail.com>
2020-07-10 00:04:04 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -15,5 +15,5 @@ def join_nested_strings(src)
   end
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
-  p str
+  p sentence
 end
